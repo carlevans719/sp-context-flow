973c68b54cd27c024bf4880868f9451d6edf94d4
Carl <carl@cevanapps.com>
2024-04-29 16:13:50 +0100

    tidy up default.aspx

--- LowTrustWeb/Pages/Default.aspx.cs
+++ LowTrustWeb/Pages/Default.aspx.cs
@@ -30,29 +30,40 @@ namespace LowTrustWeb
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Get Sharepoint site URL from query
             Uri spUri = new Uri(Request.QueryString["SPHostUrl"]);
+
+            // pull the context token off the request body
             string contextTokenString = TokenHelper.GetContextTokenFromRequest(Request);
             if (contextTokenString != null)
             {
+                // validate + parse context token
                 SharePointContextToken contextToken = TokenHelper.ReadAndValidateContextToken(contextTokenString, Request.Url.Authority);
 
+                // User+Add-In token
                 string accessToken = TokenHelper.GetAccessToken(contextToken, spUri.Authority).AccessToken;
-                string addinOnlyAccessToken = TokenHelper.GetAppOnlyAccessToken(contextToken.TargetPrincipalName, spUri.Authority, contextToken.Realm).AccessToken;
-                ClientContext clientContext = TokenHelper.GetClientContextWithAccessToken(spUri.ToString(), accessToken);
-                //clientContext.Load(clientContext.Site, site => site.Re);
-                //clientContext.ExecuteQuery();
 
+                // Add-In only token (not used)
+                string addinOnlyAccessToken = TokenHelper.GetAppOnlyAccessToken(contextToken.TargetPrincipalName, spUri.Authority, contextToken.Realm).AccessToken;
+
+                // CSOM context
+                ClientContext clientContext = TokenHelper.GetClientContextWithAccessToken(spUri.ToString(), accessToken);
+
+                // Test loading roles from api
                 string permissionLevelName = "Carl";
                 Web web = clientContext.Web;
                 clientContext.Load(web, items => items.RoleDefinitions);
                 clientContext.ExecuteQuery();
                 RoleDefinition role = web.RoleDefinitions.Where(item => item.Name == permissionLevelName).FirstOrDefault();
+
+                bool failed = false;
                 try
                 {
                     if (null != role)
                     {
                         clientContext.Load(role);
                         clientContext.ExecuteQuery();
+                        // Should fail if USER doesn't have the correct permissions
                         role.DeleteObject();
                         clientContext.ExecuteQuery();
                         Console.WriteLine("Permission Level [{0}] Removed", permissionLevelName);
@@ -61,76 +72,18 @@ namespace LowTrustWeb
                 catch (Exception exception)
                 {
                     Console.WriteLine(exception.Message);
+                    Response.Write(string.Format('Failed - {0}', exception.Message));
+                    failed = true;
                 }
 
+                if (failed != true) {
+                    Response.Write('Success');
+                }
 
-                Response.Write(accessToken);
-                Response.Write(addinOnlyAccessToken);
                 return;
             }
+
             Response.Write("Error");
-            //Uri spUri = new Uri("https://sharepoint.cevanapps.com");
-            //string code = Request.QueryString["code"];
-            //var token = TokenHelper.GetAccessToken(code, TokenHelper.SharePointPrincipal, spUri.Authority, TokenHelper.GetRealmFromTargetUrl(spUri), new Uri("https://myapp.app-host.cevanapps.com/Pages/Default.aspx")).AccessToken;
-            //Response.Write(token);
-            //ClientContext clientContext = TokenHelper.GetClientContextWithAuthorizationCode(spUri.ToString(), code, new Uri("https://myapp.app-host.cevanapps.com/Pages/Default.aspx"));
-
-            //clientContext.Load(clientContext.Web, web => web.Title);
-
-            //clientContext.ExecuteQuery();
-            //Response.Write(clientContext.Web.Title);
         }
     }
 }
-
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.UI;
-//using System.Web.UI.WebControls;
-//using LowTrustWeb;
-
-////using Microsoft.SharePoint.Samples;
-//using Microsoft.SharePoint.Client;
-
-//namespace LowTrustWeb
-//{
-//    public partial class Default : System.Web.UI.Page
-//    {
-//        protected void Page_Load(object sender, EventArgs e)
-//        {
-//            Uri sharePointSiteUrl = new Uri("https://sharepoint.cevanapps.com/");
-
-//            if (Request.QueryString["code"] != null)
-//            {
-//                TokenCache.UpdateCacheWithCode(Request, Response, sharePointSiteUrl);
-//            }
-
-//            if (!TokenCache.IsTokenInCache(Request.Cookies))
-//            {
-//                Response.Redirect(TokenHelper.GetAuthorizationUrl(sharePointSiteUrl.ToString(), "Web.Write"));
-//            }
-//            else
-//            {
-//                string refreshToken = TokenCache.GetCachedRefreshToken(Request.Cookies);
-//                string accessToken =
-//                TokenHelper.GetAccessToken(
-//                            refreshToken,
-//                            "00000003-0000-0ff1-ce00-000000000000",
-//                            sharePointSiteUrl.Authority,
-//                            TokenHelper.GetRealmFromTargetUrl(sharePointSiteUrl)).AccessToken;
-
-//                using (ClientContext context =
-//                        TokenHelper.GetClientContextWithAccessToken(sharePointSiteUrl.ToString(),
-//                                                                    accessToken))
-//                {
-//                    context.Load(context.Web);
-//                    context.ExecuteQuery();
-//                    TokenHelper.GetAppOnlyAccessToken();
-//                    Response.Write("<p>" + context.Web.Title + "</p>");
-//                }
-//            }
-//        }
-//    }
-//}
